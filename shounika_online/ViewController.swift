41ced436318fdb35913d860c93a19cb3a33fa211
Himi Sato <hiro86biro@gmail.com>
2016-11-06 17:52:31 +0900

    Initial Commit

---
+++ shounika_online/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shounika_online
+//
+//  Created by Himi Sato on 2016/11/06.
+//  Copyright © 2016 Himi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
