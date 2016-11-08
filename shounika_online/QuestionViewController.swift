186db38e364b5862ce4661b7e7adc9f666d68cf8
Himi Sato <hiro86biro@gmail.com>
2016-11-08 11:54:32 +0900

    added quwstions

---
+++ shounika_online/QuestionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  questionViewController.swift
+//  shounika_online
+//
+//  Created by Himi Sato on 2016/11/07.
+//  Copyright © 2016 Himi Sato. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
--- shounika_online/questionViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  questionViewController.swift
-//  shounika_online
-//
-//  Created by Himi Sato on 2016/11/07.
-//  Copyright © 2016 Himi Sato. All rights reserved.
-//
-
-import Foundation
