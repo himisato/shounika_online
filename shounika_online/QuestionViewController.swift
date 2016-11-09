42629f3391a6a1c2e1a0f770de0c9e74e25d9b04
Himi Sato <hiro86biro@gmail.com>
2016-11-09 19:26:21 +0900

    added some questions list

--- shounika_online/QuestionViewController.swift
+++ shounika_online/QuestionViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let questionType = ["熱は何度ありますか？", "吐いていますか？", "下痢はしていますか？", "名前を呼んだ時、反応はありますか？", "食欲はありますか？", "湿疹等、皮膚に疾患が見られますか？"]
+    
+    @IBOutlet weak var questionTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,25 @@ class QuestionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionType.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = questionType[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(tabel: UITableView, didSelectRowAtInexpPath indexPath: NSIndexPath){
+        print(questionType[indexPath.row])
+    }
+    
+    
+    
     
     
 }
